239ac04c7435f6f080fbab6e25805bf5cd21ab4c
bittib010 <77796831+bittib010@users.noreply.github.com>
2021-02-26 13:10:05 +0100

    Try fix difference
    from local html opened vs githubpages

--- javascript.js
+++ javascript.js
@@ -1,25 +1,3 @@
-/*************************
- * For Jump instructions *
- *************************/
-
-/* var highlight2 = document.getElementsByClassName("jump");
-
-for (var i = 0; i < highlight.length; i++) {
-    highlight[i].addEventListener("mouseover", highlightOEM);
-    highlight[i].addEventListener("mouseout", DonthighlightOEM);
-}
-
-function highlightOEM() {
-    for (var i = 0; i < highlight.length; i++) {
-        highlight[i].classList.add("bytehighlight");
-    }
-}
-function DonthighlightOEM() {
-    for (var i = 0; i < highlight.length; i++) {
-        highlight[i].classList.remove("bytehighlight");
-    }
-} */
-
 /***************************
  * For OEM bytes           *
  ***************************/
