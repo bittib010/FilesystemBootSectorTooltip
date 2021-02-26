0720e3d0a76652a82a3f5e2433e85c37c14f4fda
bittib010 <77796831+bittib010@users.noreply.github.com>
2021-02-26 13:05:19 +0100

    Added some text to jump

--- javascript.js
+++ javascript.js
@@ -1,26 +1,43 @@
+/*************************
+ * For Jump instructions *
+ *************************/
 
-// https://stackoverflow.com/questions/12786810/hover-on-element-and-highlight-all-elements-with-the-same-class
+/* var highlight2 = document.getElementsByClassName("jump");
 
-var classes = ["OEM", "two", "three"]; //list of your classes
-var elms = {};
-var n = {}, nclasses = classes.length;
-function changeColor(classname, color) {
-    var curN = n[classname];
-    for(var i = 0; i < curN; i ++) {
-        elms[classname][i].style.backgroundColor = color;
+for (var i = 0; i < highlight.length; i++) {
+    highlight[i].addEventListener("mouseover", highlightOEM);
+    highlight[i].addEventListener("mouseout", DonthighlightOEM);
+}
+
+function highlightOEM() {
+    for (var i = 0; i < highlight.length; i++) {
+        highlight[i].classList.add("bytehighlight");
     }
 }
-for(var k = 0; k < nclasses; k ++) {
-    var curClass = classes[k];
-    elms[curClass] = document.getElementsByClassName(curClass);
-    n[curClass] = elms[curClass].length;
-    var curN = n[curClass];
-    for(var i = 0; i < curN; i ++) {
-        elms[curClass][i].onmouseover = function() {
-            changeColor(this.className, "yellow");
-        };
-        elms[curClass][i].onmouseout = function() {
-            changeColor(this.className, "white");
-        };
+function DonthighlightOEM() {
+    for (var i = 0; i < highlight.length; i++) {
+        highlight[i].classList.remove("bytehighlight");
     }
-};
+} */
+
+/***************************
+ * For OEM bytes           *
+ ***************************/
+var highlight = document.getElementsByClassName("oem");
+
+for (var i = 0; i < highlight.length; i++) {
+    highlight[i].addEventListener("mouseover", highlightOEM);
+    highlight[i].addEventListener("mouseout", DonthighlightOEM);
+}
+
+function highlightOEM() {
+    for (var i = 0; i < highlight.length; i++) {
+        highlight[i].classList.add("bytehighlight");
+    }
+}
+function DonthighlightOEM() {
+    for (var i = 0; i < highlight.length; i++) {
+        highlight[i].classList.remove("bytehighlight");
+    }
+}
+
